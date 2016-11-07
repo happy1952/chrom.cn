6448968a8d038df4ef88d9ac9330ec99867d0ff2
Char <782845979@qq.com>
2016-11-07 17:13:23 +0800

    add template:shan

---
+++ application/views/shan/footer.inc.php
@@ -0,0 +1,13 @@
+<a href="#" id="back-to-top"><i class="fa fa-angle-up"></i></a>
+<script src="<?php echo STATICPATH; ?>static/js/jquery.3.1.1.min.js"></script>
+<script src="<?php echo STATICPATH; ?>static/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo STATICPATH; ?>static/js/ghost-url.min.js"></script>
+<script type="text/javascript">
+	ghost.init({
+		clientId: "ghost-frontend",
+		clientSecret: "26b0e31d612d"
+	});
+</script>
+<script src="http://cdn.bootcss.com/fitvids/1.1.0/jquery.fitvids.min.js"></script>
+<script src="http://cdn.bootcss.com/highlight.js/9.0.0/highlight.min.js"></script>
+<script src="<?php echo STATICPATH; ?>static/js/main.js"></script>
---
+++ application/views/shan/header.inc.php
@@ -0,0 +1,25 @@
+<?php 
+if(!defined('STATICPATH')){
+
+	define('STATICPATH', (base_url() == '') ? '../' : trim(base_url(), '/').'/');
+}
+?>
+<title>Laravel-简洁、优雅的PHP开发框架(PHP Web Framework)。- Laravel中文网</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Laravel是一套简洁、优雅的PHP Web开发框架(PHP Web Framework) -- Laravel中文网" />
+<meta name="keywords" content="Laravel中文社区,php框架,laravel中文网,php framework,restful routing,laravel,laravel php">
+<meta name="HandheldFriendly" content="True" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link rel="stylesheet" type="text/css" href="<?php echo STATICPATH; ?>static/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo STATICPATH; ?>static/css/basic.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/highlight.js/9.0.0/styles/vs.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo STATICPATH; ?>static/css/screen.css" />
+<link rel="alternate" type="application/rss+xml" title="Laravel-简洁、优雅的PHP开发框架(PHP Web Framework)。- Laravel中文网（Laravel中国社区）" href="http://www.golaravel.com/rss/" />
+<script>
+  var _hmt = _hmt || [];
+</script>
+
