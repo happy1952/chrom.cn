e93bf4c38dd11c1c28fb0d7bc693316a76d93ac1
Char <782845979@qq.com>
2016-11-08 10:24:11 +0800

    change index

--- application/views/shan/index.php
+++ application/views/shan/index.php
@@ -13,8 +13,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <h1>Chao Room</h1>
-                    <h2>成功总是偏爱那些执着于梦想的人</h2>
+                    <h1 style="color: #FFF; font-size: 48px;">ChaoRoom</h1>
+                    <h2 style="color: #FFF; font-size: 30px;">成功总是偏爱那些执着于梦想的人</h2>
                 </div>
             </div>
         </div>
@@ -23,7 +23,7 @@
 
     <!-- start navigation -->
     <nav class="main-navigation">
-        <div class="container">
+        <!-- <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="navbar-header">
@@ -34,16 +34,16 @@
                     </div>
                     <div class="collapse navbar-collapse" id="main-menu">
                         <ul class="menu">
-						    <li class="nav-current" role="presentation"><a href="/">首页</a></li>
-						    <li  role="presentation"><a href="http://lumen.golaravel.com" title="Lumen中文文档"  target="_blank">Lumen</a></li>
-						    <li  role="presentation"><a href="http://wenda.golaravel.com" title="Laravel问答社区"  target="_blank">问答社区</a></li>
-						    <li  role="presentation"><a href="/laravel/docs/" title="Laravel 中文文档" target="_blank">中文文档</a></li>
-						    <li  role="presentation"><a href="/post/laravel-documents-offline-package/" title="下载 Laravel 中文文档离线版">下载离线文档</a></li>
-						</ul>   
+                            <li class="nav-current" role="presentation"><a href="/">首页</a></li>
+                            <li  role="presentation"><a href="#" title="Linux"  target="_blank">Linux</a></li>
+                            <li  role="presentation"><a href="#" title="PHP"  target="_blank">PHP</a></li>
+                            <li  role="presentation"><a href="#" title="前端" target="_blank">前端</a></li>
+                            <li  role="presentation"><a href="#" title="关于" target="_blank">关于</a></li>
+						</ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </nav>
     <!-- end navigation -->
 
@@ -56,14 +56,11 @@
 					<article id=70 class="post">
 
 					    <div class="post-head">
-					        <h1 class="post-title"><a href="/post/laravel-5-3-is-now-released/">Laravel 5.3 正式发布</a></h1>
+					        <h1 class="post-title"><a href="#">文章标题</a></h1>
 					        <div class="post-meta">
-					            <span class="author">作者：<a href="/author/wangsai/">王赛</a></span> &bull;
+					            <span class="author">作者：<a href="#">王赛</a></span> &bull;
 					            <time class="post-date" datetime="2016年8月24日星期三凌晨2点04分" title="2016年8月24日星期三凌晨2点04分">2016年8月24日</time>
 					        </div>
-					    </div>
-					    <div class="featured-media">
-					        <a href="/post/laravel-5-3-is-now-released/"><img src="http://image.golaravel.com/9/a9/c367f99a8366ca86f7b889da25022.png" alt="Laravel 5.3 正式发布"></a>
 					    </div>
 					    <div class="post-content">
 					        <p>Laravel 项目组自豪地宣布 Laravel 5.3 正式发布了 ！5.3 版本中的新增特性主要集中在提升开发速度，通过增强常见任务的开箱即用功能提升开发效率。 此版本是常规发布版本，提供六个月的 bug 修复补丁和一年的安全补丁。当前，Laravel 5.1 是最新的 LTS（长期支持） 版本，提供两年的 bug</p>
@@ -117,24 +114,69 @@
 					</nav>
                 </main>
                 <aside class="col-md-4 sidebar">
+                    
+                    <!-- start tag cloud widget -->
+                    <div class="widget">
+                        <h4 class="title">搜索</h4>
+                        <div class="content community">
+                            <form>
+                                <p>
+                                    <input style="padding: 3px 7px 7px 7px;" type="text" name="search">
+                                    <input class="btn btn-default" type="submit" value="SEARCH">
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- end tag cloud widget -->
+
+                    <!-- start tag cloud widget -->
+                    <div class="widget">
+                        <h4 class="title">分类目录</h4>
+                        <div class="content community">
+                            <p><a href="#" title="Linux" target="_blank">Linux&nbsp;(31)</a></p>
+                            <p><a href="#" title="PHP" target="_blank">PHP&nbsp;(59)</a></p>
+                            <p><a href="#" title="Java" target="_blank">Java&nbsp;(12)</a></p>
+                            <p><a href="#" title="C/C++" target="_blank">C/C++&nbsp;(2)</a></p>
+                            <p><a href="#" title="JavaScript" target="_blank">JavaScript&nbsp;(36)</a></p>
+                            <p><a href="#" title="计算机原理" target="_blank">计算机原理&nbsp;(14)</a></p>
+                            <p><a href="#" title="网络" target="_blank">网络&nbsp;(3)</a></p>
+                        </div>
+                    </div>
+                    <!-- end tag cloud widget -->
+
 					<!-- start tag cloud widget -->
 					<div class="widget">
-						<h4 class="title">社区</h4>
+						<h4 class="title">近期文章</h4>
 						<div class="content community">
-							<p>QQ群：462694081</p>
-							<p><a href="http://wenda.golaravel.com/" title="Laravel中文网问答社区" target="_blank" onclick="_hmt.push(['_trackEvent', 'big-button', 'click', '问答社区'])"><i class="fa fa-comments"></i> 问答社区</a></p>
+							<p><a href="http://wenda.golaravel.com/" title="继续谈网络游戏的同步问题" target="_blank">继续谈网络游戏的同步问题</a></p>
+                            <p><a href="http://wenda.golaravel.com/" title="Laravel中文网问答社区" target="_blank">Laravel中文网问答社区</a></p>
+                            <p><a href="http://wenda.golaravel.com/" title="Laravel中文网问答社区" target="_blank">Laravel中文网问答社区</a></p>
+                            <p><a href="http://wenda.golaravel.com/" title="继续谈网络游戏的同步问题" target="_blank">继续谈网络游戏的同步问题</a></p>
+                            <p><a href="http://wenda.golaravel.com/" title="Laravel中文网问答社区" target="_blank">Laravel中文网问答社区</a></p>
 						</div>
 					</div>
-					<!-- end tag cloud widget -->	
+					<!-- end tag cloud widget -->
 
-					<!-- start widget -->
-					<div class="widget">
-						<h4 class="title">下载</h4>
-						<div class="content download">
-							<a href="/download/" class="btn btn-default btn-block" onclick="_hmt.push(['_trackEvent', 'big-button', 'click', '下载 Laravel & Lumen'])">Laravel &amp; Lumen 一键安装包下载</a>
-						</div>
-					</div>
-					<!-- end widget -->	
+                    <!-- start tag cloud widget -->
+                    <div class="widget">
+                        <h4 class="title">文章归档</h4>
+                        <div class="content community">
+                            <p><a href="#" title="Linux" target="_blank">2016年十一月&nbsp;(31)</a></p>
+                            <p><a href="#" title="PHP" target="_blank">2016年十二月&nbsp;(59)</a></p>
+                        </div>
+                    </div>
+                    <!-- end tag cloud widget -->
+
+                    <!-- start tag cloud widget -->
+                    <div class="widget">
+                        <h4 class="title">近期评论</h4>
+                        <div class="content community">
+                            <p><a href="#" title="Linux" target="_blank">兓: </a>工程是一门科学，科学是严谨的，态度赞一个</p>
+                            <p><a href="#" title="PHP" target="_blank">langzi:</a> @asd 我就想我刚上大学的时候怎么就没看到，不过那时候看到也没用，那 时候自己没有认识到！现在毕业了，各种恶补</p>
+                            <p><a href="#" title="Linux" target="_blank">兓: </a>工程是一门科学，科学是严谨的，态度赞一个</p>
+                        </div>
+                    </div>
+                    <!-- end tag cloud widget -->
 
 					<!-- start tag cloud widget -->
 					<div class="widget">
@@ -170,64 +212,17 @@
     <footer class="main-footer">
         <div class="container">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-md-12">
                     <div class="widget">
-                        <h4 class="title">最新文章</h4>
-                        <div class="content recent-post">
-                            <div class="recent-single-post">
-                                <a href="/post/laravel-5-3-is-now-released/" class="post-title">Laravel 5.3 正式发布</a>
-                                <div class="date">2016年8月24日</div>
-                            </div>
-                            <div class="recent-single-post">
-                                <a href="/post/laracon-laravel-5-3-recap/" class="post-title">Laracon 开发者大会快报：听 Taylor Otwell  讲解 Laravel 5.3 的新特性</a>
-                                <div class="date">2016年7月28日</div>
-                            </div>
-                            <div class="recent-single-post">
-                                <a href="/post/laravel-turns-five/" class="post-title">Laravel 五岁了</a>
-                                <div class="date">2016年6月12日</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="widget">
-                        <h4 class="title">标签云</h4>
-                        <div class="content tag-cloud">
-                            <a href="/tag/laravel/">Laravel</a>
-                            <a href="/tag/getting-started-with-laravel/">Laravel入门教程</a>
-                            <a href="/tag/laravel-5/">Laravel 5</a>
-                            <a href="/tag/laravel-5-2/">Laravel 5.2</a>
-                            <a href="/tag/eloquent/">Eloquent</a>
-                            <a href="/tag/xin-ban-ben-fa-bu/">新版本发布</a>
-                            <a href="/tag/laravel-5-1/">Laravel 5.1</a>
-                            <a href="/tag/laravle5/">laravle5</a>
-                            <a href="/tag/composer/">Composer</a>
-                            <a href="/tag/php/">PHP</a>
-                            <a href="/tag/orm/">ORM</a>
-                            <a href="/tag/artisan/">Artisan</a>
-                            <a href="/tag/lumen/">Lumen</a>
-                            <a href="/tag/lts/">LTS</a>
-                            <a href="/tag/installation/">安装</a>
-                            <a href="/tag-cloud/">...</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-4">
-                    <div class="widget">
-                        <h4 class="title">合作伙伴</h4>
+                        <h4 class="title">友情链接</h4>
                         <div class="content tag-cloud friend-links">
-                            <a href="http://www.bootcss.com" title="Bootstrap中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'bootcsscom'])" target="_blank">Bootstrap中文网</a>
-                            <a href="http://www.bootcdn.cn" title="开放CDN服务" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'bootcdncn'])" target="_blank">开放CDN服务</a>
-                            <a href="http://www.gruntjs.net" title="Grunt中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'gruntjsnet'])" target="_blank">Grunt中文网</a>
-                            <a href="http://www.gulpjs.com.cn/" title="Gulp中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'gulpjscomcn'])" target="_blank">Gulp中文网</a>
+                            <a href="http://www.bootcss.com" title="Bootstrap中文网" target="_blank">Bootstrap中文网</a>
+                            <a href="http://www.bootcdn.cn" title="开放CDN服务" target="_blank">开放CDN服务</a>
+                            <a href="http://www.gruntjs.net" title="Grunt中文网" target="_blank">Grunt中文网</a>
+                            <a href="http://www.gulpjs.com.cn/" title="Gulp中文网" target="_blank">Gulp中文网</a>
                             <hr>
-                            <a href="http://lodashjs.com/" title="Lodash中文文档" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'lodashjscom'])" target="_blank">Lodash中文文档</a>
-                            <a href="http://www.jquery123.com/" title="jQuery中文文档" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'jquery123com'])" target="_blank">jQuery中文文档</a>
-                            <hr>
-                            <a href="https://www.upyun.com/" title="又拍云" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'upyun'])" target="_blank">又拍云</a>
-                            <a href="http://www.ucloud.cn/" title="UCloud" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'ucloud'])" target="_blank">UCloud</a>
-                            <a href="http://www.lecloud.com/" title="乐视云" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'lecloud'])" target="_blank">乐视云</a>
+                            <a href="http://github.com/" title="GitHub" target="_blank">GitHub</a>
+                            <a href="http://www.jquery123.com/" title="jQuery中文文档" target="_blank">jQuery中文文档</a>
                         </div>
                 	</div>
                 </div>
@@ -239,8 +234,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <span>Copyright &copy; <a href="http://www.golaravel.com/">Laravel 中文网</a></span> |
-                    <span><a href="http://www.miibeian.gov.cn/" target="_blank">京ICP备11008151号</a></span> |
+                    <span>Copyright &copy; <a href="#">ChaoDong个人网站</a></span> |
+                    <span><a href="#" target="_blank">京ICP备11008151号</a></span> |
                     <span>京公网安备11010802014853</span>
                 </div>
             </div>
