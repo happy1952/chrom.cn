7574790f3b93986d855355ad8b9a2085d9673a78
Char <782845979@qq.com>
2016-11-28 17:14:50 +0800

    change application/config/config_baseURL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://chrom.cn/';
+$config['base_url'] = 'http://www.chrom.cn/';
 
 /*
 |--------------------------------------------------------------------------
