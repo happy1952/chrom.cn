07ff4f32c7e47f8ffec8a28ecde2c45094aeb508
Char <782845979@qq.com>
2016-12-15 16:52:51 +0800

    修改前端继承父类

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -1,10 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Categories extends CI_Controller {
+class Categories extends MY_Home {
+
+	public function __construct(){
+
+		parent::__construct();
+	}
 
 	public function index(){
 
 		$this->load->view('shan/categories');
 	}
 }
+
+?>
--- application/controllers/Tagclouds.php
+++ application/controllers/Tagclouds.php
@@ -1,10 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tagclouds extends CI_Controller {
+class Tagclouds extends MY_Home {
+
+	public function __construct(){
+
+		parent::__construct();
+	}
 
 	public function index(){
 
 		$this->load->view('shan/tagcloud');
 	}
 }
+
+?>
